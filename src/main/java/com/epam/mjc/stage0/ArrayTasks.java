8bbe8847f15f2686b64685f0d8425871873200d1
Makar <hoteng789@gmail.com>
2023-07-29 11:56:39 +0400

    Work with Arrays

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -1,5 +1,7 @@
 package com.epam.mjc.stage0;
 
+
+
 /**
  * Here are the tasks for working with the arrays.
  * <p>
@@ -11,7 +13,8 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-
+     String[] seasons =  {"winter", "spring", "summer", "autumn"};
+     return  seasons;
     }
 
     /**
@@ -25,7 +28,12 @@ public class ArrayTasks {
      * length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
-
+        int[] returnArray = new int[length];
+        for (int i = 0; i <length; i++)
+        {
+            returnArray[i] = i+1;
+        }
+        return  returnArray;
     }
 
     /**
@@ -37,7 +45,12 @@ public class ArrayTasks {
      * arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            sum += arr[i];
+        }
+        return  sum;
     }
 
     /**
@@ -50,7 +63,15 @@ public class ArrayTasks {
      * arr = [5, -3, -4],   number = 10    ->  -1
      */
     public int findIndexOfNumber(int[] arr, int number) {
+        for (int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] == number)
+            {
+                return  i;
+            }
+        }
 
+        return  -1;
     }
 
     /**
@@ -63,7 +84,14 @@ public class ArrayTasks {
      * arr = ["pineapple", "apple", "pen"] -> ["pen", "apple", "pineapple"]
      */
     public String[] reverseArray(String[] arr) {
+        String[] reverseArr = new String[arr.length];
 
+        for (int i = arr.length-1; i >= 0; i--)
+        {
+            reverseArr[arr.length - i - 1] = arr[i];
+        }
+
+        return  reverseArr;
     }
 
     /**
@@ -78,6 +106,31 @@ public class ArrayTasks {
      * arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
+        int lengthNewArray = 0;
+        for(int i = 0; i<arr.length; i++)
+        {
+            if(arr[i] > 0){
+                lengthNewArray++;
+            }
+        }
+        int[] positiveArr = new int[lengthNewArray];
+        lengthNewArray = 0;
+        for(int i = 0; i <arr.length; i++)
+        {
+            if(lengthNewArray == positiveArr.length)
+            {
+                return  positiveArr;
+            }
+
+            if(arr[i] > 0){
+                positiveArr[lengthNewArray] = arr[i];
+                lengthNewArray++;
+
+            }
+
+        }
+
+        return positiveArr;
 
     }
 
@@ -92,6 +145,82 @@ public class ArrayTasks {
      * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
     public int[][] sortRaggedArray(int[][] arr) {
+        int lowestNumber = 0;
+        int index = 0;
+        int[] temp;
+        for(int i = 0 ; i < arr.length ; i ++)
+        {
+           arr[i] = sortArray(arr[i]);
+        }
 
+        for(int i = 0; i < arr.length; i++)
+        {
+            lowestNumber = arr[i].length;
+            index = i;
+            for (int j = i; j <arr.length ; j ++)
+            {
+                if(lowestNumber > arr[j].length )
+                {
+                    lowestNumber = arr[j].length;
+                    index = j;
+                }
+            }
+            if(index != i )
+            {
+                temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
+            }
+
+        }
+        return  arr;
+    }
+
+    public  int[] sortArray(int[] arr)
+    {
+
+        if(arr.length != 1 )
+        {
+            int[] firstPartArray = new int[arr.length/2];
+            int[] secondPartArray = new int[arr.length - firstPartArray.length];
+
+            System.arraycopy(arr, 0, firstPartArray,0,firstPartArray.length);
+            System.arraycopy(arr, firstPartArray.length, secondPartArray,0 ,secondPartArray.length);
+
+            firstPartArray = sortArray(firstPartArray);
+            secondPartArray = sortArray(secondPartArray);
+
+
+            int countFirst = 0;
+            int countSecond = 0;
+
+            while (countFirst < firstPartArray.length && countSecond < secondPartArray.length)
+            {
+                if(firstPartArray[countFirst] > secondPartArray[countSecond])
+                {
+                    arr[countFirst+countSecond] = secondPartArray[countSecond];
+                    countSecond++;
+                }else {
+                    arr[countFirst+countSecond] = firstPartArray[countFirst];
+                    countFirst++;
+                }
+            }
+
+            if (countFirst < firstPartArray.length)
+            {
+                arr[countFirst+countSecond] = firstPartArray[countFirst];
+                countFirst++;
+            }
+
+            if (countSecond < secondPartArray.length)
+            {
+                arr[countFirst+countSecond] = secondPartArray[countSecond];
+                countSecond++;
+            }
+
+            return arr;
+
+        }
+        return arr;
     }
 }
